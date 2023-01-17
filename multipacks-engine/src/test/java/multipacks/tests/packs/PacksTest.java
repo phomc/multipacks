deea41e8f5c791dde7e313bf17a385f126614d58
nahkd123 <nahkd123@gmail.com>
2023-01-17 21:42:03 +0700

    Add test for LocalPack

---
+++ multipacks-engine/src/test/java/multipacks/tests/packs/PacksTest.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2020-2022 MangoPlex
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package multipacks.tests.packs;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.nio.file.Path;
+
+import org.junit.jupiter.api.Test;
+
+import multipacks.packs.LocalPack;
+import multipacks.packs.meta.PackIndex;
+
+/**
+ * @author nahkd
+ *
+ */
+class PacksTest {
+	@Test
+	void testPacksFromJar() throws Exception {
+		Path rootPath = Path.of(this.getClass().getClassLoader().getResource("multipacksAssets").toURI());
+
+		LocalPack pack = new LocalPack(rootPath);
+		pack.loadFromStorage();
+
+		PackIndex index = pack.getIndex();
+		assertEquals("sample-pack", index.name);
+		assertEquals("PhoMC", index.author);
+
+		assertNotNull(pack.createVfs(false).get(new multipacks.vfs.Path("assets/multipacks/models/sample_model.json")));
+	}
+}
