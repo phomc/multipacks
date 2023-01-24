e88efc68b14adab74b5cd769e8f447fffdc2e28f
nahkd123 <nahkd123@gmail.com>
2023-01-24 13:51:30 +0700

    Add files watching to build command

--- multipacks-cli/src/main/java/multipacks/cli/commands/BuildCommand.java
+++ multipacks-cli/src/main/java/multipacks/cli/commands/BuildCommand.java
@@ -20,6 +20,12 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardWatchEventKinds;
+import java.nio.file.WatchEvent;
+import java.nio.file.WatchKey;
+import java.nio.file.WatchService;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import multipacks.bundling.BundleResult;
 import multipacks.bundling.Bundler;
@@ -51,6 +57,9 @@ public class BuildCommand extends Command {
 	@Option(value = "--override", helpDescription = "Override existing output file (if exists)")
 	public boolean override = false;
 
+	@Option(value = { "--watch", "-W" }, helpDescription = "Rebuild pack when changes are made (override will also be enabled)")
+	public boolean watch = false;
+
 	public BuildCommand(MultipacksCommand parent) {
 		this.platform = parent.platform;
 		helpName = "build";
@@ -65,7 +74,51 @@ public class BuildCommand extends Command {
 		if (!Files.exists(packDir.resolve(LocalPack.FILE_INDEX))) throw new CommandException("Missing " + LocalPack.FILE_INDEX + ": " + packDir);
 
 		System.out.println("Building '" + packDir + "' ...");
+		if (watch) System.out.println("Watch mode is enabled!");
+		buildOnce(packDir, watch || override);
 
+		if (watch) {
+			platform.getLogger().info("Getting files watcher ready...");
+
+			try (LoggingStage stage = platform.getLogger().newStage("Watch", "Initialize watch service")) {
+				WatchService service = packDir.getFileSystem().newWatchService();
+				WatchKey key = packDir.register(service, StandardWatchEventKinds.ENTRY_CREATE, StandardWatchEventKinds.ENTRY_DELETE, StandardWatchEventKinds.ENTRY_MODIFY);
+
+				stage.newStage("Initalize stopper");
+				AtomicBoolean stopping = new AtomicBoolean(false);
+				Runtime.getRuntime().addShutdownHook(new Thread() {
+					@Override
+					public void run() {
+						platform.getLogger().info("Shutting down watch service...");
+						stopping.set(true);
+					}
+				});
+
+				stage.newStage("Watching");
+				AtomicBoolean built = new AtomicBoolean(true);
+				while (!stopping.get()) {
+					List<WatchEvent<?>> event = key.pollEvents();
+
+					if (event.size() == 0) {
+						if (built.get()) {
+							Thread.sleep(100);
+							continue;
+						}
+
+						System.out.println("Changes finalized, building...");
+						buildOnce(packDir, true);
+						built.set(true);
+					} else {
+						built.set(false);
+					}
+				}
+			} catch (IOException | InterruptedException e) {
+				throw new CommandException("An error occured while initalizing watch", e);
+			}
+		}
+	}
+
+	private void buildOnce(Path packDir, boolean override) {
 		try (LoggingStage stage = platform.getLogger().newStage("Build", "Initialize", 3)) {
 			LocalPack pack = new LocalPack(packDir);
 
@@ -78,7 +131,7 @@ public class BuildCommand extends Command {
 			File outputFile = new File(outputDestination != null? outputDestination : (pack.getIndex().name + "-v" + pack.getIndex().packVersion.toStringNoPrefix() + ".zip"));
 			if (outputFile.exists()) {
 				if (!override) throw new CommandException("File is already exists: " + outputFile + ". Override that file with --override=true option.");
-				platform.getLogger().warning("File is already exists: {}. Deleting... (--override=true option)", outputFile);
+				platform.getLogger().warning("File is already exists: {}. Deleting... (--override=true option or watch mode)", outputFile);
 				outputFile.delete();
 			}
 
