892343552a7b4eb0cbcbe4eb12b4c75f5c79c527
nahkd123 <nahkd123@gmail.com>
2023-01-25 10:03:18 +0700

    Add helper methods

---
+++ multipacks-spigot/src/main/java/multipacks/spigot/helpers/GlyphHelper.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2020-2022 MangoPlex
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package multipacks.spigot.helpers;
+
+import multipacks.modifier.builtin.glyphs.Glyph;
+import net.md_5.bungee.api.chat.TextComponent;
+
+/**
+ * @author nahkd
+ *
+ */
+public class GlyphHelper {
+	public static TextComponent toBungee(Glyph glyph) {
+		TextComponent tc = new TextComponent(String.valueOf(glyph.assigned));
+		tc.setFont(glyph.font.id.toString());
+		return tc;
+	}
+}
