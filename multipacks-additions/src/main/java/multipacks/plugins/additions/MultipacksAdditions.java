7c6514fff0c0539788801c2c5d6b7252dce6c403
nahkd123 <nahkd123@gmail.com>
2023-01-25 13:58:08 +0700

    Multipacks Additions

---
+++ multipacks-additions/src/main/java/multipacks/plugins/additions/MultipacksAdditions.java
@@ -0,0 +1,10 @@
+package multipacks.plugins.additions;
+
+import multipacks.platform.Platform;
+import multipacks.plugins.Plugin;
+
+public class MultipacksAdditions extends Plugin {
+    @Override
+    public void onInit(Platform platform) {
+    }
+}
